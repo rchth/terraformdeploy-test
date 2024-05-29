d0e6cf4e5e1e45d68e91c735b90edfadb222c2b2
ROCH Théo <theo.roch@wavestone.com>
2024-05-29 09:57:31 +0200

    using tfvars

--- modules/storage-account/variables.tf
+++ modules/storage-account/variables.tf
@@ -1,18 +1,9 @@
-variable "storage_account_name1" {
+variable "storage_account_name" {
   description = "The name of the storage account."
   type        = string
 }
 
-variable "resource_group_name1" {
-  description = "The name of the resource group."
-  type        = string
-}
-variable "storage_account_name2" {
-  description = "The name of the storage account."
-  type        = string
-}
-
-variable "resource_group_name2" {
+variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
 }

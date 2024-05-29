e5188cdd107da517d2b0fb41fd4110488ccaea37
ROCH Théo <theo.roch@wavestone.com>
2024-05-29 09:23:02 +0200

    Deploy the first storage account with module adapt workflows

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "storage_account" {
-  source              = "./module/storage-account"
+  source              = "./modules/storage-account"
   storage_account_name = "deploymentfromgithub123"
   resource_group_name  = "rg-test-for-tf"
   location             = "francecentral"

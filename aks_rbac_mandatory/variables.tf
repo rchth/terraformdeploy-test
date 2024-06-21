e87baffc2c875cc5cf80adfe6fa382dc9f24130d
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 11:14:52 +0200

    Rename workflow

---
+++ aks_rbac_mandatory/variables.tf
@@ -0,0 +1,43 @@
+variable "location" {
+  description = "The location of the resource group."
+  type        = string
+}
+
+variable "prefix" {
+  description = "Prefix for naming resources."
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for the AKS cluster."
+  type        = string
+}
+
+variable "node_count" {
+  description = "The number of nodes in the default node pool."
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine."
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "azure_admin_group_object_id" {
+  description = "The Object ID of the Azure AD group that will have admin access to the AKS cluster."
+  type        = string
+  default     = ""
+}
+
+variable "authentication_method" {
+  description = "The way to authenticate to Kubernetes cluster"
+  type = string
+}

e2fce97068abbbda90705a8ca8a4453eb2a21f11
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 11:17:28 +0200

    Reshape variable & fix errors

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,21 +1,21 @@
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
+  location            = var.location
+  resource_group_name = "rg-test-for-tf"
 }
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "${var.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.aks.name
+  resource_group_name  = "rg-test-for-tf"
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.prefix}-aks"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
+  location            = var.location
+  resource_group_name = "rg-test-for-tf"
   dns_prefix          = var.dns_prefix
 
   default_node_pool {

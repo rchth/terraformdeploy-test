89c7616bfd27a42ca0632135599c0198730f573a
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 18:18:45 +0200

    Deploy with fixed based on doc

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "aks_subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = "rg-test-for-tf"
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {

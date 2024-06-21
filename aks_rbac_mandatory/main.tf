2c50264a62eda4341a8ed69ee5bb4173defc82b2
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 11:12:33 +0200

    Rename workflow

--- aks_rbac_mandatory/main.tf
+++ aks_rbac_mandatory/main.tf
@@ -1,7 +1,7 @@
 module "azurerm_kubernetes_cluster"  {
   source              = "../modules/aks"    
   location            = var.location
-  prefix              = .var.prefix
+  prefix              = var.prefix
   dns_prefix          = var.dns_prefix
   node_count          = var.node_count
   vm_size             = var.vm_size

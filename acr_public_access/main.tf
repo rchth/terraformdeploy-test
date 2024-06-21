ad7ae6b59f922bf14e731496be169cb32c738868
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 19:10:04 +0200

    Test with ACR for public deployment

--- acr_public_access/main.tf
+++ acr_public_access/main.tf
@@ -1,6 +1,6 @@
 module "acr" {
   source = "../modules/acr"
-  name = var.acr_name
+  acr_name = var.acr_name
   sku  = var.sku
   public_network_access_enabled = var.public_network_access_enabled
 }

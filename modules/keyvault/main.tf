480d4488ba7e17e801fd0e640533889ca14869e8
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 19:42:43 +0200

    Add test for keyvault rbac and public

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -5,4 +5,5 @@ resource "azurerm_key_vault" "keyvault" {
     sku_name = var.sku
     enable_rbac_authorization = var.enable_rbac_authorization
     public_network_access_enabled = var.public_network_access_enabled
+    tenant_id = "029393e1-83bc-49e3-8f9e-37d6e2e36e48"
 }

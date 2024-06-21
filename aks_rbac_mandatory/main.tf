e7091eff00be9e27c8edc3f860c7fd36c93d1fb1
ROCH Théo <theo.roch@wavestone.com>
2024-06-21 17:28:58 +0200

    test deploy full rbac aks

--- aks_rbac_mandatory/main.tf
+++ aks_rbac_mandatory/main.tf
@@ -7,4 +7,6 @@ module "azurerm_kubernetes_cluster"  {
   vm_size             = var.vm_size
   azure_admin_group_object_id = "870104dc-08b5-4226-a7b1-cd65b16018bd"
   local_account_disabled = var.local_account_disabled
+  isazurerbacenabled = var.isazurerbacenabled
+  isrbacenabled = var.isrbacenabled
 }
